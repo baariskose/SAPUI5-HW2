945a6251379d84a2ce07b6343a17cad66f356e57
baariskose <61475377+baariskose@users.noreply.github.com>
2022-10-27 21:21:26 +0300

    Show Employee With Card
    Showed Employee With Card into Grid Layout

---
+++ employee-app-card/employee-list/webapp/controller/View1.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("com.smod.employeelist.controller.View1", {
+            onInit: function () {
+
+            },
+            convertImagePath: function(imageData){
+                var sTrimmedData = imageData.substr(104);
+                return "data:image/bmp;base64," + sTrimmedData;
+            }
+        });
+    });
